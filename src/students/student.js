a9dfecab40fb9078a58b3da1b1018aaaec152751
Edwin Moz <edwin.moz@icloud.com>
2024-02-20 12:21:02 -0600

    students page

--- src/students/student.js
+++ src/students/student.js
@@ -1,9 +1,10 @@
-export const Student = ({ student, setStudent }) => {
+export const Student = ({ grades, student, setStudent }) => {
 
     return (
         <section className="flex flex-col items-center justify-center">
             <img className="h-60" src={`https://api.dicebear.com/7.x/big-smile/svg?seed=${student.firstName}%20${student.lastName}`} alt="" />
             <p className="text-3xl">{student.firstName} {student.lastName}</p>
+            <p>{grades.find((grade) => grade.id === student.gradeId)?.name} grade</p>
         </section>
     )
 }
